76090f9863b0f80f91db223dcc51076bf36a423e
Didier Gerschheimer <didier.gerschheimer@gmail.com>
2022-12-17 11:50:52 +0100

    player prefab

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public Vector2 inputMovement = new Vector2();
+
+    private void FixedUpdate()
+    {
+        HandleMovements();
+    }
+
+    private void HandleMovements()
+    {
+        
+    }
+}
