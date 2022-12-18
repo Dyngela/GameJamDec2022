2ba3cd2ed0c8d2fec792182155e6d8f901d48b34
Didier Gerschheimer <didier.gerschheimer@gmail.com>
2022-12-18 23:08:38 +0100

    add timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class GameManager : MonoBehaviour
 {
     public bool isGamePaused = false;
     private float previousTimeScale = 0f;
     public static GameManager instance;
+
+    [Header("Game State")]
+    [SerializeField] private PlankPickup _plankPickup;
+    [SerializeField] private Collider2D _exitColliderBlock;
+    [SerializeField] private ExitZone _exitArea;
+    [SerializeField] private TMP_Text _currentTimeText;
+    private TimeSpan _currentTime;
+    
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        _currentTime = TimeSpan.Zero;
     }
 
     // Update is called once per frame
     private void Update()
     {
         if (isGamePaused) return;
+        
+        // Update time
+        _currentTime = _currentTime.Add(TimeSpan.FromSeconds(Time.deltaTime));
+        if (_currentTimeText != null)
+            _currentTimeText.text = _currentTime.ToString();
+        
         
         if (Player.instance.sanity <= 0.0f)
         {
@@ -50,5 +72,10 @@ public class GameManager : MonoBehaviour
             Time.timeScale = previousTimeScale;
             isGamePaused = false;
         }
+    }
+
+    public void OnPlankPickup()
+    {
+        throw new System.NotImplementedException();
     }
 }
